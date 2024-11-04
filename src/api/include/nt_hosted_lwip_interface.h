f86d069586b1b3fb0fbfd068de8e2c43987b910b
quic_ryzhang <quic_ryzhang@quicinc.com>
2024-11-04 10:02:43 +0800

    CR3965395 Change the license of lwip hosted interface header file to BSD
    Change-Id: Ic2170fb0712c9c136926fc8352955284f68f9ea8
    (cherry picked from commit 42596a3288e1c2358f6217b73c5eb8b587d4dbb0)

--- src/api/include/nt_hosted_lwip_interface.h
+++ src/api/include/nt_hosted_lwip_interface.h
@@ -1,6 +1,6 @@
 /**
  *Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
- *SPDX-License-Identifier: ISC
+ *SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 /*
  * nt_hosted_lwip_interface.h
